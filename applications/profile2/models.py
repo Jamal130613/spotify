ca458e7908fd0210631c7e5fe92c41e8fb230f61
Jamal130613 <liyapark.com@gmail.com>
2022-08-09 19:29:30 +0600

    first commit

---
+++ applications/profile2/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+
+class Profile(models.Model):
+    user=models.OneToOneField(User, null=True, on_delete=models.CASCADE)
+    bio=models.TextField(null=True, blank=True)
+    profile_pic = models.ImageField(null=True, blank=True, upload_to="images/profile/")
+    facebook = models.CharField(max_length=50, null=True, blank=True)
+    twitter = models.CharField(max_length=50, null=True, blank=True)
+    instagram = models.CharField(max_length=50, null=True, blank=True)
+    
+def __str__(self):
+    return str(self.user)
